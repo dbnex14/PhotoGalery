7c8f48545e8f1b6bdc1cf58f079ae52486df7429
dbnex14 <dbnex14@gmail.com>
2014-11-02 13:21:15 -0800

    Add logic to fetch images on demand.

--- app/src/main/java/com/learning/dino/photogalery/PhotoGaleryFragment.java
+++ app/src/main/java/com/learning/dino/photogalery/PhotoGaleryFragment.java
@@ -1,13 +1,17 @@
 package com.learning.dino.photogalery;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -20,21 +24,54 @@ public class PhotoGaleryFragment extends Fragment {
 
     GridView mGridView;
     ArrayList<GaleryItem> mItems;
+    ThumbnailDownloader<ImageView> mThumbnailThread;  //this is a background HandlerThread
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setRetainInstance(true);  //retain the fragment
-
         new FetchItemsTask().execute();
+
+        //pass main thread's handler to ThumbnailDownloader background thread.  Remember, since this
+        //handler is created in main thread, it will attach itsself to the main thread looper.
+        mThumbnailThread = new ThumbnailDownloader<ImageView>(new Handler());
+        //set listener to set the returning bitmaps on the ImageView handles and update main thread
+        //from the background thread
+        mThumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>(){
+            public void onThumbnailDownloaded(ImageView imageView, Bitmap thumbnail){
+                if (isVisible()){
+                    //We guard call to setImageBitmap with Fragment.isVisible() to ensure that we
+                    //are not setting the image on a stale ImageView
+                    imageView.setImageBitmap(thumbnail);
+                }
+            }
+        });
+        mThumbnailThread.start();
+        mThumbnailThread.getLooper();
+        Log.i(TAG, "Background thread ThumbnailDownloader started");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_photo_galery, container, false);
         mGridView = (GridView)v.findViewById(R.id.gridView);
+
         setupAdapter();
         return v;
+    }
+
+    @Override
+    public void onDestroyView(){
+        //Clean out downloader when view is destroyed.
+        super.onDestroy();
+        mThumbnailThread.clearQueue();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mThumbnailThread.quit();
+        Log.i(TAG, "Background thread ThumbnailDownloader destroyed");
     }
 
     void setupAdapter(){
@@ -43,7 +80,8 @@ public class PhotoGaleryFragment extends Fragment {
         }
 
         if (mItems != null){
-            mGridView.setAdapter(new ArrayAdapter<GaleryItem>(getActivity(), android.R.layout.simple_gallery_item, mItems));
+            mGridView.setAdapter(new GaleryItemAdapter(mItems));
+            //mGridView.setAdapter(new ArrayAdapter<GaleryItem>(getActivity(), android.R.layout.simple_gallery_item, mItems));
         } else {
             mGridView.setAdapter(null);
         }
@@ -72,6 +110,26 @@ public class PhotoGaleryFragment extends Fragment {
         protected void onPostExecute(ArrayList<GaleryItem> items){
             mItems = items;
             setupAdapter();
+        }
+    }
+
+    private class GaleryItemAdapter extends ArrayAdapter<GaleryItem>{
+
+        public GaleryItemAdapter(ArrayList<GaleryItem> items){
+            super(getActivity(), 0, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            if (convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.galery_item, parent, false);
+            }
+
+            ImageView imageView = (ImageView)convertView.findViewById(R.id.galery_item_imageView);
+            imageView.setImageResource(R.drawable.blank_image);
+            GaleryItem item = getItem(position);
+            mThumbnailThread.queueThumbnail(imageView, item.getUrl());
+            return convertView;
         }
     }
 }
---
+++ app/src/main/java/com/learning/dino/photogalery/ThumbnailDownloader.java
@@ -0,0 +1,116 @@
+package com.learning.dino.photogalery;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dbulj on 02/11/2014.
+ * ThumbnailDownloader is how we schedkule work on the background thread from the main thread using
+ * ThumbnailDownloader's handler.
+ * Token is a generic argument for some object used to id each download.
+ */
+public class ThumbnailDownloader<Token> extends HandlerThread {
+
+    private static final String TAG = "ThubnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 0; //Message 'what' used to describe the message
+
+    Handler mHandler; //Message 'target' handler that will process the message
+    Map<Token, String> requestMap = Collections.synchronizedMap(new HashMap<Token, String>()); //Message 'obj' object sent with message
+
+    //Tie ThumbnailDownloader via mResponseHandler to the main thread's looper.
+    Handler mResponseHandler; //handler tied to the main thread's looper handler passed in c-tor.
+
+    //Listener interface to communicate the responses with and do UI work with the returniing Bitmaps.
+    Listener<Token> mListener;
+    public interface Listener<Token>{
+        void onThumbnailDownloaded(Token token, Bitmap thumbnail);
+    }
+    public void setListener(Listener<Token> listener){
+        mListener = listener;
+    }
+
+    //public ThumbnailDownloader(){
+    public ThumbnailDownloader(Handler responseHandler){
+        super(TAG);
+        mResponseHandler = responseHandler;
+    }
+
+    @SuppressLint("HandlerLeak")
+    @Override
+    protected void onLooperPrepared(){
+        //onLooperPrepared is called before the Looper checks the queue for the first time, so
+        //we create here our Handler
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                //so check for message type, retrieve the Token and pass it to handleRequest()
+                if (msg.what == MESSAGE_DOWNLOAD){
+                    @SuppressWarnings("unchecked")
+                    Token token = (Token)msg.obj;
+                    Log.i(TAG, "Got a request for url: " + requestMap.get(token));
+                    handleRequest(token);
+                }
+            }
+        };
+    }
+
+    public void queueThumbnail(Token token, String url){
+        Log.i(TAG, "Got an Url: " + url);
+        requestMap.put(token, url); //add passed in Token URL pair to the map
+        //then obtain a message, give it the token as its object, and send it off to be put on the
+        //message queue
+        mHandler.obtainMessage(MESSAGE_DOWNLOAD, token).sendToTarget();
+    }
+
+    private void handleRequest(final Token token){
+        //here is where downloading happens
+        try{
+            final String url = requestMap.get(token);
+            if (url == null){
+                return;
+            }
+
+            byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+
+            //use BitmapFactory to construct a bitmap with the array of bytes returned
+            final Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+
+            //Handler.post() is a convenience method for posting Message.  Because mResponseHandler
+            //is associated with the main (UI) thread's Looper, this UI update will run on the main
+            //thread.
+            mResponseHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    //Since GridView recycles its views, we check that each Token gets teh correct
+                    //image even if another request has been made in the meantime.
+                    if (requestMap.get(token) != url){
+                        return;
+                    }
+                    requestMap.remove(token);
+                    mListener.onThumbnailDownloaded(token, bitmap);
+                }
+            });
+        }catch(IOException ioe){
+            Log.e(TAG, "Error downloading image", ioe);
+        }
+    }
+
+    //If user rotates the device, ThumbnailDownloader may be handing on the invalid ImageViews.  So,
+    //this method will clean all the requests our of message queue.
+    public void clearQueue(){
+        mHandler.removeMessages(MESSAGE_DOWNLOAD);
+        requestMap.clear();
+    }
+}
+
